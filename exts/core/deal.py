f706a0319f4d4819b37d8151361da28180784318
sushi-chaaaan <sushi_code@outlook.jp>
2022-05-05 04:00:41 +0900

    fix UI

--- exts/core/deal.py
+++ exts/core/deal.py
@@ -24,9 +24,9 @@ class Deal(commands.Cog):
     async def user(
         self,
         ctx: commands.Context,
-        target: discord.Member,
+        target: discord.Member | discord.User,
     ):
-        joined = dt_to_str(target.joined_at) if target.joined_at else "取得できませんでした"
+        """ユーザー情報照会用コマンド"""
         avatar_url = (
             target.default_avatar.url
             if target.default_avatar == target.display_avatar
@@ -41,20 +41,30 @@ class Deal(commands.Cog):
         embed.set_footer(text=f"{dt_to_str()}")
         embed.set_thumbnail(url=avatar_url)
         embed.add_field(
+            name="Bot?",
+            value=f"{'True' if target.bot else 'False'}",
+        )
+        embed.add_field(
             name="アカウント作成日時",
             value=f"{dt_to_str(target.created_at)}",
         )
-        embed.add_field(
-            name="サーバー参加日時",
-            value=f"{joined}",
-        )
-        roles = sorted(target.roles, key=lambda role: role.position, reverse=True)
-        text = "\n".join([role.mention for role in roles])
-        embed.add_field(
-            name=f"所持ロール({len(roles)})",
-            value=text,
-            inline=False,
-        )
+        if isinstance(target, discord.Member):
+            joined = dt_to_str(target.joined_at) if target.joined_at else "取得できませんでした"
+            embed.add_field(
+                name="サーバー参加日時",
+                value=f"{joined}",
+            )
+            roles = sorted(target.roles, key=lambda role: role.position, reverse=True)
+            text = "\n".join([role.mention for role in roles])
+            embed.add_field(
+                name=f"所持ロール({len(roles)})",
+                value=text,
+                inline=False,
+            )
+        else:
+            embed.description = (
+                f"\N{Warning Sign}このサーバーにいないユーザーです。\n対象ユーザー: {target.mention}"
+            )
         await ctx.send(embeds=[embed])
         return
 
