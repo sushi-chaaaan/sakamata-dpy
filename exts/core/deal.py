c376567fd24444cd814d718a3bb56d6b0d7d2c3e
sushi-chaaaan <sushi_code@outlook.jp>
2022-05-05 03:17:33 +0900

    進捗

--- exts/core/deal.py
+++ exts/core/deal.py
@@ -5,6 +5,7 @@ from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 from model.color import Color
+from tools.confirm import Confirm
 from tools.dt import dt_to_str
 from tools.logger import getMyLogger
 
@@ -14,6 +15,7 @@ logger = getMyLogger(__name__)
 class Deal(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.confirm = Confirm(bot)
         load_dotenv()
 
     @commands.hybrid_command(name="user")
@@ -30,7 +32,7 @@ class Deal(commands.Cog):
             if target.default_avatar == target.display_avatar
             else target.display_avatar.replace(size=1024, static_format="webp")
         )
-        await ctx.defer(ephemeral=True)
+        await ctx.defer()
         embed = discord.Embed(
             title="ユーザー情報照会結果",
             description=f"対象ユーザー: {target.mention}",
@@ -61,24 +63,40 @@ class Deal(commands.Cog):
     @app_commands.guild_only()
     @app_commands.describe(target="Choose user to kick")
     async def kick(self, interaction: discord.Interaction, target: discord.Member):
-        await interaction.response.defer(ephemeral=True)
-        if not isinstance(target, discord.Member):
-            await interaction.followup.send(
-                content="target is not discord.Member", ephemeral=True
-            )
+        await interaction.response.defer()
+
+        # get channel and role
+        ctx = await commands.Context.from_interaction(interaction)
+        channel = ctx.channel
+        if not isinstance(channel, discord.abc.Messageable):
             return
-        text = f"{target.mention} was kicked"
-        context = await commands.Context.from_interaction(interaction)
-        try:
-            await interaction.guild.kick(target)  # type: ignore -> checked by Discord server side
-        except discord.Forbidden as e:
-            text = "Failed to kick member: Missing permissions"
-            logger.error(text, exc_info=e)
-        except Exception as e:
-            text = "Failed to kick member: Unknown error"
-            logger.error(text, exc_info=e)
-        finally:
-            await interaction.followup.send(content=text)
+        role = ctx.guild.get_role(int(os.environ["ADMIN"]))  # type: ignore -> checked by Discord server side
+        if not role:
+            return
+
+        # do confirm
+        text = f"{target.mention}をkickしますか？"
+        res = await self.confirm.confirm(
+            ctx=ctx,
+            watch_role=role,
+            text=text,
+            run_num=1,
+            stop_num=1,
+        )
+        if res:
+            try:
+                await interaction.guild.kick(target)  # type: ignore -> checked by Discord server side
+            except discord.Forbidden as e:
+                text = "Failed to kick member: Missing permissions"
+                logger.error(text, exc_info=e)
+            except Exception as e:
+                text = "Failed to kick member: Unknown error"
+                logger.error(text, exc_info=e)
+            finally:
+                await ctx.send(content=text)
+                return
+        else:
+            await ctx.send(content="Canceled")
             return
 
 
---
+++ tools/confirm.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+accept_emoji = "\N{Heavy Large Circle}"
+reject_emoji = "\N{Cross Mark}"
+
+
+class Confirm:
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        load_dotenv()
+
+    async def confirm(
+        self,
+        *,
+        ctx: commands.Context,
+        watch_role: discord.Role | list[discord.Role],
+        text: str,
+        run_num: int,
+        stop_num: int,
+    ) -> bool:
+
+        # setup role list
+        watch_roles = (
+            [watch_role] if isinstance(watch_role, discord.Role) else watch_role
+        )
+        watch_ids = [r.id for r in watch_roles]
+        watch_mentions = "\n".join([f"{role.mention}" for role in watch_roles])
+
+        # send message to watch
+        send_text = (
+            "【コマンド実行確認】\n------------------------\n"
+            + text
+            + f"\n------------------------\nコマンド承認:{watch_mentions}\n実行に必要な承認人数: {str(run_num)}\n中止に必要な承認人数: {str(stop_num)}"
+        )
+        target = await ctx.send(content=send_text)
+        await target.add_reaction(accept_emoji)
+        await target.add_reaction(reject_emoji)
+
+        # wait for reaction
+        def check(payload: discord.RawReactionActionEvent):
+            return (
+                payload.message_id == target.id
+                and payload.emoji.name in [accept_emoji, reject_emoji]
+                and payload.member is not None
+                and payload.member.id != self.bot.user.id  # type: ignore -> it has no problem if bot is successfully started
+                and [r.id for r in payload.member.roles if r.id in watch_ids]
+                is not None
+            )
+
+        payload: discord.RawReactionActionEvent = await self.bot.wait_for(
+            "raw_reaction_add", check=check
+        )
+        self._done = True
+        if payload.emoji.name == accept_emoji:
+            # accept
+            return True
+        else:
+            # reject
+            return False
