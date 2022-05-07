023b358beb13c221eaa73add670a465eb6a7f590
sushi-chaaaan <sushi_code@outlook.jp>
2022-05-07 23:23:33 +0900

    release add-role-to-thread

--- exts/core/thread.py
+++ exts/core/thread.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 import discord
@@ -53,6 +54,88 @@ class ThreadSys(commands.Cog):
             return
         else:
             return
+
+    @app_commands.command(name="add-role-to-thread")
+    @app_commands.guilds(discord.Object(id=int(os.environ["GUILD_ID"])))
+    @app_commands.guild_only()
+    @app_commands.describe(thread_id="Input thread_id to add role's members")
+    @app_commands.describe(role="Choose role to add to thread")
+    async def add_member_to_thread(
+        self,
+        interaction: discord.Interaction,
+        thread_id: str,
+        role: discord.Role,
+    ):
+        logger.info(
+            f"{interaction.user}[ID: {interaction.user.id}] used add-role-to-thread command"
+        )
+
+        await interaction.response.defer(thinking=True)
+
+        # get thread
+        thread = interaction.guild.get_thread(int(thread_id))  # type: ignore -> checked by Discord server side
+        if not thread:
+            await interaction.followup.send(content="スレッドが見つかりませんでした。")
+            return
+
+        before_count = len(thread.members)
+
+        await interaction.followup.send(content="ロールメンバーの取得を開始します。")
+
+        # get chunked members
+        members = role.members
+        chunk: list[str] = []
+        _chunk: str = ""
+
+        for member in members:
+
+            # メンションを生成
+            m = f"<@{member.id}>"
+
+            # 追加後の文字数が2000文字以下なら_chunkに直接追加する
+            # そうでなければ現在の_chunkをchunkに追加したあと,あたらしい_chunkをmで初期化
+            if len(_chunk) + len(m) < 2000:
+                _chunk += m
+            else:
+                chunk.append(_chunk)
+                # mで初期化
+                _chunk = m
+        # 最後の_chunkをchunkに追加
+        chunk.append(_chunk)
+
+        await interaction.followup.send(
+            content=f"ロールメンバーの取得を完了しました。\n{str(len(role.members))}人を{str(len(chunk))}回に分けて\n{thread.mention}に追加します。"
+        )
+
+        # send message to edit
+        try:
+            msg = await thread.send(content="test message")
+        except Exception as e:
+            logger.exception(f"{thread.name} is not accessible", exc_info=e)
+            await interaction.followup.send(
+                content=f"{thread.mention}にアクセスできません。\n処理を停止します。"
+            )
+            return
+
+        # add members to thread
+        err_count = 0
+        for i, text in enumerate(chunk):
+            try:
+                await msg.edit(content=f"{text}")
+            except Exception as e:
+                logger.exception(
+                    f"failed to edit message: {msg.id},Index:{str(i)}",
+                    exc_info=e,
+                )
+                err_count += 1
+            finally:
+                await asyncio.sleep(0.50)
+        after_count = len(thread.members)
+        await interaction.followup.send(
+            content=f"処理を完了しました。\n追加したメンバー数:{str(after_count - before_count)}/{len(role.members)}\nエラー回数:{str(err_count)}"
+        )
+        await msg.delete(delay=180.0)
+        return
 
     @app_commands.command(name="thread-board")
     @app_commands.guilds(discord.Object(id=int(os.environ["GUILD_ID"])))
