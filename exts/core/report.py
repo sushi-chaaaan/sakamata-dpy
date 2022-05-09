4108425c2e2a753f78240ecb57c4e88a8b2e1198
sushi-chaaaan <sushi_code@outlook.jp>
2022-05-09 22:44:59 +0900

    add report

--- exts/core/report.py
+++ exts/core/report.py
@@ -23,11 +23,16 @@ class Report(commands.Cog):
             guild_ids=[int(os.environ["GUILD_ID"])],
         )
         self.bot.tree.add_command(self.user_ctx_menu_report)
+        self.bot.tree.add_command(self.message_ctx_menu_report)
 
     async def cog_unload(self) -> None:
         self.bot.tree.remove_command(
             self.user_ctx_menu_report.name,
             type=self.user_ctx_menu_report.type,
+        )
+        self.bot.tree.remove_command(
+            self.message_ctx_menu_report.name,
+            type=self.message_ctx_menu_report.type,
         )
 
     @app_commands.guild_only()
@@ -40,7 +45,7 @@ class Report(commands.Cog):
         # get text input
         value = await tracker.track_modal(
             title=f"{user}を管理者に通報しますか？",
-            custom_id="exts.core.report.report",
+            custom_id="exts.core.report.report_user",
             min_length=1,
             max_length=2000,
             ephemeral=True,
@@ -50,6 +55,29 @@ class Report(commands.Cog):
             return
 
         await ctx.send(f"{user}を管理者に通報しました。", ephemeral=True)
+        return
+
+    @app_commands.guild_only()
+    async def report_message(
+        self, interaction: discord.Interaction, message: discord.Message
+    ) -> None:
+        pass
+        ctx = await commands.Context.from_interaction(interaction)
+        tracker = TextInputTracker(ctx=ctx)
+
+        # get text input
+        value = await tracker.track_modal(
+            title="選択したメッセージを管理者に通報しますか？",
+            custom_id="exts.core.report.report_message",
+            min_length=1,
+            max_length=2000,
+            ephemeral=True,
+        )
+        if not value:
+            await ctx.send(content="正しく入力されませんでした。")
+            return
+
+        await ctx.send("メッセージを管理者に通報しました。", ephemeral=True)
         return
 
 
