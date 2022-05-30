79f56010100a7d7fb2f462ca16f4001c398fd501
sushi-chaaaan <sushi_code@outlook.jp>
2022-05-31 04:30:09 +0900

    fix index

--- src/poll.py
+++ src/poll.py
@@ -1,28 +1,26 @@
 import json
 
 emoji = {
-    "1": "\N{Large Red Circle}",
-    "2": "\N{Large Green Circle}",
-    "3": "\N{Large Orange Circle}",
-    "4": "\N{Large Blue Circle}",
-    "5": "\N{Large Brown Circle}",
-    "6": "\N{Large Purple Circle}",
-    "7": "\N{Large Red Square}",
-    "8": "\N{Large Green Square}",
-    "9": "\N{Large Orange Square}",
-    "10": "\N{Large Blue Square}",
-    "11": "\N{Large Brown Square}",
-    "12": "\N{Large Purple Square}",
-    "13": "\N{Large Orange Diamond}",
-    "14": "\N{Large Blue Diamond}",
-    "15": "\N{Heavy Black Heart}",
-    "16": "\N{Green Heart}",
-    "17": "\N{Orange Heart}",
-    "18": "\N{Blue Heart}",
-    "19": "\N{Brown Heart}",
-    "20": "\N{Purple Heart}",
+    "0": "\N{Large Red Circle}",
+    "1": "\N{Large Green Circle}",
+    "2": "\N{Large Orange Circle}",
+    "3": "\N{Large Blue Circle}",
+    "4": "\N{Large Brown Circle}",
+    "5": "\N{Large Purple Circle}",
+    "6": "\N{Large Red Square}",
+    "7": "\N{Large Green Square}",
+    "8": "\N{Large Orange Square}",
+    "9": "\N{Large Blue Square}",
+    "10": "\N{Large Brown Square}",
+    "11": "\N{Large Purple Square}",
+    "12": "\N{Large Orange Diamond}",
+    "13": "\N{Large Blue Diamond}",
+    "14": "\N{Heavy Black Heart}",
+    "15": "\N{Green Heart}",
+    "16": "\N{Orange Heart}",
+    "17": "\N{Blue Heart}",
+    "18": "\N{Brown Heart}",
+    "19s": "\N{Purple Heart}",
 }
 
-json.dump(emoji, open("emoji.json", "a"))
-
-poll_emoji_list = []
+json.dump(emoji, open("./poll_emoji.json", "a"))
