7e9676133d71f5431b4d6c498ed90c1db2b8c663
sushi-chaaaan <sushi_code@outlook.jp>
2022-06-04 10:33:29 +0900

    refactoring / use external config file

--- main.py
+++ main.py
@@ -5,111 +5,88 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from components.confirm import ConfirmView
 from exts.core.inquiry import InquiryView
 from model.color import Color
 from tools.dt import dt_to_str
+from tools.finder import Finder
+from tools.io import read_json
 from tools.logger import getMyLogger
-
-logger = getMyLogger(__name__)
-
-
-EXT_LIST = [
-    "exts.core.entrance",
-    "exts.core.deal",
-    "exts.core.dm",
-    "exts.core.error",
-    "exts.core.inquiry",
-    "exts.core.member_count",
-    "exts.core.message",
-    "exts.core.report",
-    "exts.core.thread",
-    "exts.core.word_alert",
-    "exts.logging.listener",
-    "exts.poll",
-    "exts.utils",
-]
-
-intents = discord.Intents.all()
-intents.typing = False
 
 
 class MyBot(commands.Bot):
-    def __init__(self, intents: discord.Intents, **kwargs):
-        super().__init__(
-            command_prefix="//",
-            intents=intents,
-            **kwargs,
-        )
-        self.persistent_views_added = False
+    def __init__(self):
+        intents = discord.Intents.all()
+        intents.typing = False
+        super().__init__(command_prefix="//", intents=intents)
+
+        # setup logger
+        self.logger = getMyLogger(__name__)
+
+        # setup vars
         self.failed_extensions = []
         self.failed_views = []
         self.synced_commands = 0
 
     async def setup_hook(self):
+        self.load_config()
+        await self.load_exts()
+        await self.sync_commands()
+        await self.setup_view()
 
+    async def on_ready(self):
+        await self.send_boot_message()
+        self.logger.info("Bot is ready")
+
+    def load_config(self):
+        config = read_json("config/config.json")
+        self.ext_list = config["ext_list"]
+
+    async def load_exts(self):
         # load extensions
-        for ext in EXT_LIST:
+        for ext in self.ext_list:
             try:
                 await self.load_extension(ext)
             except Exception as e:
-                logger.error(f"Failed to load extension {ext}", exc_info=e)
+                self.logger.error(f"Failed to load extension {ext}", exc_info=e)
                 self.failed_extensions.append(ext)
             else:
-                logger.info(f"Loaded extension {ext}")
+                self.logger.info(f"Loaded extension {ext}")
         if not self.failed_extensions:
             self.failed_extensions = ["None"]
 
+    async def sync_commands(self):
         # sync command
         try:
             cmd = await self.tree.sync(
                 guild=discord.Object(id=int(os.environ["GUILD_ID"]))
             )
             self.synced_commands = len(cmd)
-            logger.info(f"{self.synced_commands} commands synced")
+            self.logger.info(f"{self.synced_commands} commands synced")
         except Exception as e:
-            logger.error(f"Failed to sync command tree: {e}")
+            self.logger.error(f"Failed to sync command tree: {e}")
 
+    def load_persistent(self):
+        dict = read_json("config/persistent_view.json")
+        return [ConfirmView(custom_id=c_id) for c_id in dict["confirm"]]
+
+    async def setup_view(self):
         # add persistent_view
-        VIEWS: list[discord.ui.View] = [InquiryView()]
-        if not self.persistent_views_added:
-            for v in VIEWS:
-                try:
-                    self.add_view(v)
-                except TypeError as e:
-                    logger.error(
-                        f"[TypeError]\nFailed to add persistent view {v}", exc_info=e
-                    )
-                    self.failed_views.append(v.__str__())
-                except ValueError as e:
-                    logger.error(
-                        f"[ValueError]\nFailed to add persistent view {v}", exc_info=e
-                    )
-                    self.failed_views.append(v.__str__())
-                except Exception as e:
-                    logger.error(
-                        f"[Unknown Exception]\nFailed to add persistent view {v}",
-                        exc_info=e,
-                    )
-                    self.failed_views.append(v.__str__())
+        confirm = self.load_persistent()
+        VIEWS: list[discord.ui.View] = [InquiryView(), *confirm]
+        for v in VIEWS:
+            try:
+                self.add_view(v)
+            except Exception as e:
+                self.logger.error(
+                    f"Failed to add persistent view {(fv := str(v))}",
+                    exc_info=e,
+                )
+                self.failed_views.append(fv)
         if not self.failed_views:
             self.failed_views = ["None"]
-        self.persistent_views_added = True
 
-    async def on_ready(self):
-        # send boot log
-        embed = self._get_embed()
-        channel = self.get_channel(int(os.environ["LOG_CHANNEL"]))
-        if not channel:
-            channel = await self.fetch_channel(int(os.environ["LOG_CHANNEL"]))
-        if not isinstance(channel, discord.abc.Messageable):
-            logger.error(
-                f"Failed to get Messageable channel {os.environ['LOG_CHANNEL']}"
-            )
-        else:
-            await channel.send(embeds=[embed])
-        logger.info("Bot is ready")
-
-    def _get_embed(self) -> discord.Embed:
+    def get_embed(self) -> discord.Embed:
         embed = discord.Embed(
             title="Booted",
             description=f"Time: {dt_to_str()}",
@@ -144,11 +121,24 @@ class MyBot(commands.Bot):
         )
         return embed
 
+    async def send_boot_message(self):
+        # send boot log
+        embed = self.get_embed()
+        finder = Finder(self)
+        channel = await finder.search_channel(int(os.environ["LOG_CHANNEL"]))
+
+        if not isinstance(channel, discord.abc.Messageable):
+            self.logger.error(
+                f"Failed to get Messageable channel {os.environ['LOG_CHANNEL']}"
+            )
+        else:
+            await channel.send(embeds=[embed])
+
+    def run(self):
+        load_dotenv()
+        super().run(os.environ["DISCORD_BOT_TOKEN"])
+
 
 if __name__ == "__main__":
-    load_dotenv()
-    bot = MyBot(intents=intents, application_id=int(os.environ["APP_ID"]))
-    try:
-        bot.run(os.environ["DISCORD_BOT_TOKEN"])
-    except Exception as e:
-        logger.exception("Failed to run bot", exc_info=e)
+    bot = MyBot()
+    bot.run()
