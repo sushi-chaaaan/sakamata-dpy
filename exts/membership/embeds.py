9bc339d813534830160c23ef6cd0e26e16b0c1d8
sushi-chaaaan <sushi_code@outlook.jp>
2022-06-04 09:03:13 +0900

    進捗

--- exts/membership/embeds.py
+++ exts/membership/embeds.py
@@ -8,3 +8,12 @@ class EmbedBuilder:
     def __init__(self) -> None:
         load_dotenv()
         self.logger = getMyLogger(__name__)
+
+    @staticmethod
+    def member_confirm_embed(image_url: str) -> Embed:
+        embed = Embed(
+            title="メンバーシップ更新手続き",
+            color=Color.default.value,
+        )
+        embed.set_image(url=image_url)
+        return embed
--- exts/membership/membership.py
+++ exts/membership/membership.py
@@ -1,9 +1,14 @@
 import os
 
 import discord
+from components.confirm import ConfirmView
 from discord.ext import commands
 from dotenv import load_dotenv
+from model.response import ExecuteResponse
 from tools.gsheets import GSheetClient
+from tools.webhook import post_webhook
+
+from .embeds import EmbedBuilder as EB
 
 
 class MemberShip(commands.Cog):
@@ -19,6 +24,37 @@ class MemberShip(commands.Cog):
 
     async def post_to_sheet(self):
         client = GSheetClient(os.environ["SPREAD_SHEET_CRED"])
+
+    @commands.command(name="verify")
+    @commands.dm_only()
+    async def verify(self, ctx: commands.Context, attachment: discord.Attachment):
+
+        # check attachment
+        VALID_CONTENT_TYPE = ["image/png", "image/jpeg"]
+        if attachment.content_type not in VALID_CONTENT_TYPE:
+            await ctx.reply("画像を添付してください。")
+            return
+
+        # do confirm
+
+    async def do_confirm(self, image_url: str) -> bool:
+
+        # generate confirmation message
+        view = ConfirmView(
+            custom_id="exts.membership.verify.do_confirm",
+            accept="承認",
+            reject="拒否",
+            timeout=None,
+        )
+
+        embed = EB.member_confirm_embed(image_url)
+
+        # do confirm
+        await post_webhook(os.environ["MEMBER_WEBHOOK_URL"], embeds=[embed], view=view)
+        await view.wait()
+        res = view.value
+
+        # check result
 
 
 async def setup(bot: commands.Bot):
