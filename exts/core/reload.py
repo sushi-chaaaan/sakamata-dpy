7158ff7974f71baa98f5c2717477773af78a2a70
sushi-chaaaan <sushi_code@outlook.jp>
2022-06-04 10:56:46 +0900

    add reload

---
+++ exts/core/reload.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+from exts.bot import ChloeriumBot
+
+
+class Reload(commands.Cog):
+    def __init__(self, bot: ChloeriumBot):
+        self.bot = bot
+        load_dotenv()
+
+    @commands.command(name="reload")
+    @commands.has_permissions(administrator=True)
+    async def reload(self, ctx: commands.Context):
+        await self.bot.reload_exts()
+        await ctx.reply("Reloaded!!!")
+
+
+async def setup(bot: ChloeriumBot):
+    await bot.add_cog(Reload(bot))
