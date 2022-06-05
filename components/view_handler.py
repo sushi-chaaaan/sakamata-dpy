c49f153151078a4cc6df3bdae874575ebe986aba
sushi-chaaaan <sushi_code@outlook.jp>
2022-06-05 16:20:15 +0900

    fix code style

--- components/view_handler.py
+++ components/view_handler.py
@@ -3,7 +3,7 @@ from discord import ui
 
 def to_unavailable(view: ui.View) -> ui.View:
     for c in view.children:
-        if isinstance(c, ui.Button) or isinstance(c, ui.Select):
+        if isinstance(c, (ui.Button, ui.Select)):
             c.disabled = True
         else:
             continue
